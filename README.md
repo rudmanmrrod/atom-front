f061479710d649dcaddffdf5f388b1260eeae8a0
Rodrigo Boet <rudmanmrrod@gmail.com>
2024-05-29 18:32:44 -0400

    added front

---
+++ README.md
@@ -0,0 +1,16 @@
+## Frontend
+
+* Angular 17
+* Angular Material
+
+El front se orientó a subdivididir lo que angular comprende por componentes en 
+tipos pages (lo que serían las páginas generales que se usan en la navegación)
+y los componentes que la conforman (componentes en el sentido más general que usa
+javascript y otros frameworks), adicional dichos componentes estan clasificados por el
+módulo al que pertenecen.
+
+### Instalar para dev
+
+`npm i`
+
+`npm start`
---
+++ src/app/services/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment';
+// Type
+import type { User, UserResponse } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private baseURL: string = environment.dev.apiUrl + '/users/';
+
+  constructor(private http: HttpClient) {}
+
+  public checkUser(email: string) {
+    return this.http.get<User[]>(this.baseURL + email);
+  }
+
+  public createUser(email: string) {
+    return this.http.post<UserResponse>(this.baseURL, {email});
+  }
+
+  public getUserEmail() {
+    return localStorage.getItem('userEmail') || '';
+  }
+
+  public logout() {
+    localStorage.clear();
+    return true;
+  }
+}
